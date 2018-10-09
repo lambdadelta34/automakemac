03d670f5f143870e5eebf6b087970d6a03f3a83a
lambdadelta <waterdropfalls@gmail.com>
2018-10-09 17:57:34 +0300

    add kitty

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,2 +1,6 @@
 set PATH $HOME/.rbenv/shims $PATH
 status --is-interactive; and source (rbenv init -|psub)
+rbenv rehash >/dev/null ^&1
+alias gcbh="git remote prune origin ; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D"
+alias gcb="git remote prune origin ; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d"
+kitty + complete setup fish | source
