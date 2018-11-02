420978b44e31b7bd3b4d9b1fba5326932be9c290
lambdadelta <waterdropfalls@gmail.com>
2018-11-03 00:42:45 +0300

    tweak confing

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -4,3 +4,4 @@ rbenv rehash >/dev/null ^&1
 alias gcbh="git remote prune origin ; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D"
 alias gcb="git remote prune origin ; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d"
 kitty + complete setup fish | source
+fish_vi_key_bindings
