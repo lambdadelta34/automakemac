f4b605d812f40c6cfa0150ec01d8c8751f54a698
lambdadelta <waterdropfalls@gmail.com>
2019-01-29 20:49:34 +0300

    fish functions

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -3,7 +3,10 @@ set PATH $HOME/.rbenv/shims $PATH
 set -x LC_ALL en_US.UTF-8
 status --is-interactive; and source (rbenv init -|psub)
 rbenv rehash >/dev/null ^&1
-alias gcbh="git remote prune origin ; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -D"
-alias gcb="git remote prune origin ; git branch -vv | grep 'origin/.*: gone]' | awk '{print $1}' | xargs git branch -d"
 kitty + complete setup fish | source
 fish_vi_key_bindings 2>/dev/null
+set -x LC_ALL en_US.UTF-8
+set NIX_REMOTE daemon
+
+# opam configuration
+source /Users/aa/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
