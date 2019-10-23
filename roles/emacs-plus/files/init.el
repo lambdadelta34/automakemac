8304b7d0b31a56d907d629476242b6d6dd59d7ed
lambdadelta <waterdropfalls@gmail.com>
2019-10-23 17:30:53 +0300

    tweak configs

--- roles/emacs-plus/files/init.el
+++ roles/emacs-plus/files/init.el
@@ -6,6 +6,9 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(custom-safe-themes
+   (quote
+    ("4e7e04c4b161dd04dc671fb5288e3cc772d9086345cb03b7f5ed8538905e8e27" default)))
  '(package-selected-packages
    (quote
     (yasnippet-snippets yasnippet ruby-end slim-mode magit powerline which-key use-package tuareg smartparens rjsx-mode reason-mode projectile neotree haskell-mode ggtags general flycheck flx evil-surround evil-nerd-commenter evil-args dumb-jump dracula-theme counsel company-quickhelp all-the-icons ag))))
@@ -16,5 +19,6 @@
  ;; If there is more than one, they won't work right.
  '(awesome-tab-selected ((t :background "#bd93f9")))
  '(awesome-tab-unselected ((t :background "#8BE9FD")))
+ '(font-lock-type-face ((t :foreground "DarkRed")))
  '(sp-show-pair-match-content-face ((t :foreground "#00ffff" :background "#6700D4")) t)
  '(sp-show-pair-match-face ((t :foreground "#00ffff" :background "#6700D4"))))
