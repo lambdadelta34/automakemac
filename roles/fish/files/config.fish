c2871ed13a41a552cee8d372f50c12e2db0c4648
ld <lambdadelta@Koumakan.local>
2021-10-17 22:16:28 +0300

    twk

--- roles/fish/files/config.fish
+++ roles/fish/files/config.fish
@@ -1,5 +1,5 @@
 status --is-interactive; and source (rbenv init -|psub)
-rbenv rehash >/dev/null ^&1
+rbenv rehash >/dev/null 2>&1
 kitty + complete setup fish | source
 set -x LC_ALL en_US.UTF-8
 set NIX_REMOTE daemon
@@ -7,5 +7,3 @@ set NIX_REMOTE daemon
 # opam configuration
 source ~/.opam/opam-init/init.fish > /dev/null 2> /dev/null; or true
 fish_add_path /usr/local/sbin
-
-source /usr/local/opt/asdf/asdf.fish
